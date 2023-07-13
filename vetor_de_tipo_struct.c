d9a8727c8b1d70f496691d34ad8d6a385a5f29af
GabasDev <bieltoddy065@gmail.com>
2023-07-13 12:24:26 -0300

    jwf

--- vetor_de_tipo_struct.c
+++ vetor_de_tipo_struct.c
@@ -1,45 +1,40 @@
-#include<stdio.h>
-typedef struct pessoa{
+#include <stdio.h>
+typedef struct pessoa
+{
     char nome[20];
     int idade;
     int RG;
     float altura;
-}Pessoa;
+} Pessoa;
 
-    int main (void){
-    
-
+int main(void)
+{
 
     Pessoa var_pessoa[2];
-    
+
     int indice;
 
-    for (indice=0; indice <2; indice++){
+    for (indice = 0; indice < 2; indice++)
+    {
 
-    
+        printf("Digite o nome: ");
+        scanf(" %[^\n]", var_pessoa[indice].nome);
 
-    printf ("Digite o nome: ");
-    scanf (" %[^\n]", var_pessoa[indice].nome);
+        printf("Digite idade: \n");
+        scanf("%d", &var_pessoa[indice].idade);
 
-    printf ("Digite idade: \n");
-    scanf ("%d", &var_pessoa[indice].idade);
+        printf("Digite o RG: \n");
+        scanf("%d", &var_pessoa[indice].RG);
 
-    printf ("Digite o RG: \n");
-    scanf ("%d", &var_pessoa[indice].RG);
+        printf("Digite a altura: \n");
+        scanf("%f", &var_pessoa[indice].altura);
 
-    printf ("Digite a altura: \n");
-    scanf ("%f", &var_pessoa[indice].altura);
+    }
 
-    //printf ("os dados informados: \n Nome: %s \t Idade: %d \t  RG: %d \t altura: %.2f ", var_pessoa.nome, var_pessoa.idade, var_pessoa.RG, var_pessoa.altura);
-
-    }   
-
-
-    
-
-    
-    
-
-
-return 0;
+    for (indice = 0; indice < 2; indice++)
+    {
+        printf("os dados informados: \n");
+        printf(" Nome: %s \t Idade: %d \t  RG: %d \t altura: %.2f ", var_pessoa[indice].nome, var_pessoa[indice].idade, var_pessoa[indice].RG, var_pessoa[indice].altura);
+    }
+    return 0;
 }
