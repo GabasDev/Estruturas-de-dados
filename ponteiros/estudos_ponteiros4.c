c9fcc4eefdc72486ea09ec3bbf33df541f17db6d
Gabriel Ferreira <112407415+GabasDev@users.noreply.github.com>
2023-07-30 14:56:52 -0300

    Update estudos_ponteiros4.c

--- ponteiros/estudos_ponteiros4.c
+++ ponteiros/estudos_ponteiros4.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 #include<math.h>
-
+//criei a funcao de calcular o hexagono
 void calcula_hexagono (float l, float *area, float *perimetro){
     *area=(3*pow(l,2)*sqrt(3))/2;
     *perimetro=6*l;
 }
-
+// na main declarei as variaveis e chamei a funcao com os argumentos
 int main (void){
     float a;
     float p;    
