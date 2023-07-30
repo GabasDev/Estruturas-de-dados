370176198e698b8055eb6bb63ccc9fe06c42f8c0
GabasDev <bieltoddy065@gmail.com>
2023-07-30 12:30:55 -0300

    alteracao

--- praticalab/questao_revisao/quest_alocacao_com_string.c
+++ praticalab/questao_revisao/quest_alocacao_com_string.c
@@ -1,12 +1,17 @@
+#include <stdlib.h>
 #include <string.h>
-char * soma (char * vetor1, char * vetor2, int tamanho ){
-    char * vetor_soma = (char *) malloc (tamanho*sizeof(char));
-    if (vetor_soma==NULL){
-        exit (1);
+
+char* concatenarStrings(const char* string1, const char* string2) {
+    // Calcula o tamanho necessario para a nova string
+    size_t tamanho_total = strlen(string1) + strlen(string2);
+    // Aloca memoria para a nova string
+    char* resultado = (char*)malloc((tamanho_total + 1) * sizeof(char)); // +1 para o caractere nulo de terminacao
+    if (resultado == NULL) {
+        exit(1); // Tratamento de erro para falha na alocação de memória
     }
-    int contador;
-    for (contador=0; contador < tamanho; contador++){
-        vetor_soma[contador]=vetor1[contador]+vetor2[contador];
-    }
-    return (vetor_soma);
+    // Copia a primeira string para o resultado
+    strcpy(resultado, string1);
+    // Concatena a segunda string ao final do resultado
+    strcat(resultado, string2);
+    return resultado;
 }
