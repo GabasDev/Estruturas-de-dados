63270914e9e8cf042c6bf7cd35c7252658345bd6
GabasDev <bieltoddy065@gmail.com>
2023-08-01 16:13:56 -0300

    o que eu queria fazer

--- prova/prova.c
+++ prova/prova.c
@@ -1,51 +1,90 @@
-#inlcude<stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 
-typdef struct professor{
-    char nome[81], disci;
+typedef struct professor
+{
+    char nome[81], disci[81];
     int horas;
-}Professor;
+} Professor;
 
-void preenche (Professor *professor){
-    printf ("insira seu nome");
-    scanf (" %[^]c", &nome);
-    printf ("insira sua disciplina")
-    scanf (" %[^]c", &disci);
-    printf ("insira seu regime de trabalho");
-    scanf ("%f", &horas);
+void preenche(Professor **professor)
+{
+    *professor = (Professor *)malloc(sizeof(Professor));
+
+    printf("insira seu nome\n");
+    scanf(" %[^\n]", (*professor)->nome);
+
+    printf("insira sua disciplina\n");
+    scanf(" %[^\n]", (*professor)->disci);
+
+    printf("insira seu regime de trabalho\n");
+    scanf("%d", &((*professor)->horas));
 }
 
-void imprime (Professor *professor){
-    printf ("o nome %c", Professor->nome);
-    printf ("disciplina %c", Professor->disci);
-    printf ("regime de trabalho %d", Professor->horas);
+void imprime(Professor *professor)
+{
+    printf("o nome: %s\n", professor->nome);
+    printf("disciplina: %s\n", professor->disci);
+    printf("regime de trabalho: %d\n", professor->horas);
 }
 
-void altera (Professor *professor){
-    printf ("digite o novo regime de trabalho");
-    scanf ("%d", &horas);
+void altera(Professor *professor)
+{
+    printf("digite o novo regime de trabalho: ");
+    scanf("%d", &(professor->horas));
 }
-int main(){
+
+int main()
+{
     int opcao;
-    Professor *professor==NULL;
-    do {
-        printf ("O QUE DESEJA FAZER?");
-        printf ("1-PREENCHER");
-        printf ("2-IMPRIMIR");
-        printf ("3-ALTERAR");
-        scanf ("%d", &opcao);
-        switch (opcao){
-            case 1:
+    Professor *professor = NULL;
+
+    do
+    {
+        printf("O QUE DESEJA FAZER?\n");
+        printf("1-PREENCHER\n");
+        printf("2-IMPRIMIR\n");
+        printf("3-ALTERAR\n");
+        printf("4-SAIR\n");
+        scanf("%d", &opcao);
+
+        switch (opcao)
+        {
+        case 1:
             preenche(&professor);
             break;
-            case 2: 
-            imprime(*professor);
+        case 2:
+            if (professor != NULL)
+            {
+                imprime(professor);
+            }
+            else
+            {
+                printf("Professor não preenchido ainda!\n");
+            }
             break;
-            case 3:
-            altera(&professor);
+        case 3:
+            if (professor != NULL)
+            {
+                altera(professor);
+            }
+            else
+            {
+                printf("Professor não preenchido ainda!\n");
+            }
             break;
-        } 
-    }while (opcao!=3);
-    return 0; 
+        case 4:
+            // Exit the loop and end the program
+            break;
+        default:
+            printf("Opção inválida!\n");
+            break;
+        }
 
+    } while (opcao != 4);
+
+    // Free allocated memory before exiting the program
+    free(professor);
+
+    return 0;
 }
